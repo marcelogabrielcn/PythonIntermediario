38aa5611faa6fb34029cd95234328efcce07e5be
Marcelo Gabriel <marcelogabrielcn@gmail.com>
2022-02-21 16:13:17 -0300

    Add files via upload

--- 60.XDownloadVideoYouTube.py
+++ 60.XDownloadVideoYouTube.py
@@ -4,7 +4,7 @@ youtube = YouTube('<link do video>')
 for stream in youtube.streams:
     print(stream)
 
-#Baixo Vídeos mp4 format
+#Baixar Vídeos mp4 format
 #youtube.streams.get_highest_resolution().download()
 
 #Baixar apenas áudio mp4 format
