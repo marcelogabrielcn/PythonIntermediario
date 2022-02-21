c76895f47f0dbfe51fd6da83a6cc59c2c09845da
Marcelo Gabriel <marcelogabrielcn@gmail.com>
2022-02-21 17:53:19 -0300

    Add files via upload

--- 60.ExpressoesLambda.py
+++ 60.ExpressoesLambda.py
@@ -11,3 +11,31 @@ var = mult(4, 4)
 
 a = lambda x, y: x * y
 print(a(4, 4))
+
+lista = [
+    ['P1', 120],
+    ['P2', 170],
+    ['P3', 180],
+    ['P4', 20],
+    ['P5', 60]
+]
+
+
+def func(item):
+    return item[1]
+
+
+lista.sort(key=func)
+print(lista)
+
+
+lista2 = [
+    ['P1', 190],
+    ['P2', 230],
+    ['P3', 520],
+    ['P4', 70],
+    ['P5', 6230]
+]
+
+lista2.sort(key=lambda item: item[1])
+print(lista2)
