a4ddeab945e30db6347fda77eacb6008cbe1b713
Marcelo Gabriel <marcelogabrielcn@gmail.com>
2022-02-24 15:44:02 -0300

    Add files via upload

---
+++ 63.QASystem.py
@@ -0,0 +1,36 @@
+"""
+Sistema de perguntas e respostas utilizando diionários em Python
+"""
+
+print()
+print('Sistema de perguntas e respostas.')
+print('Você verá algumas perguntas, ao final escolha uma resposta que julgar correta.')
+respostas_certas = 0
+
+
+perguntas = {
+    'pergunta 1': {
+        'pergunta': 'Quanto é 2+2?',
+        'respostas': {'a': '3', 'b': '4', 'c': '6'},
+        'resposta_certa': 'b'
+    },
+    'pergunta 2': {
+        'pergunta': 'Quanto é 3x5?',
+        'respostas': {'a': '12', 'b': '18', 'c': '15'},
+        'resposta_certa': 'c'
+    }
+}
+print()
+
+for pk, pv in perguntas.items():
+    print(f'{pk}: {pv["pergunta"]}')
+
+    print('Respostas: ')
+    for rk, rv in pv['respostas'].items():
+        print(f'[{rk}]: {rv}')
+
+    resposta_user = input('Sua resposta: ')
+    print()
+
+    if resposta_user == pv['resposta_certa']:
+        print('Obaaa!!!! Você acertooou! ><><><><')
