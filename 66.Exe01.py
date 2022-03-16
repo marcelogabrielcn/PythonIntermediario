72f483a7aeef011cd446a397e8d13b49bb6794ff
Marcelo Gabriel <marcelogabrielcn@gmail.com>
2022-03-16 20:11:38 -0300

    Add files via upload

--- 66.Exe01.py
+++ 66.Exe01.py
@@ -1,3 +1,5 @@
 String = '01234567890123456789012345678901234567890123456789'
-list0 = [n for n in String.split('9')]
-print(list0, sep='.')
+n = 10
+list0 = [String[i:i+n] for i in range(0, len(String), n)]
+print('.'.join(list0))
+
