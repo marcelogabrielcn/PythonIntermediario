b223a4077850452586940a79224fcc5b9c3f9055
Marcelo Gabriel <marcelogabrielcn@gmail.com>
2022-03-30 17:49:30 -0300

    Add files via upload

---
+++ dados80.py
@@ -0,0 +1,31 @@
+"""
+Map
+"""
+
+produtos = [
+    {'nome': 'p1', 'preco': 16},
+    {'nome': 'p2', 'preco': 73},
+    {'nome': 'p3', 'preco': 56},
+    {'nome': 'p4', 'preco': 94},
+    {'nome': 'p5', 'preco': 18},
+    {'nome': 'p6', 'preco': 26},
+    {'nome': 'p7', 'preco': 84},
+    {'nome': 'p8', 'preco': 73},
+    {'nome': 'p9', 'preco': 257},
+    {'nome': 'p10', 'preco': 235}
+]
+
+pessoas = [
+    {'nome': 'Marcelo', 'Idade': 22},
+    {'nome': 'João', 'Idade': 31},
+    {'nome': 'José', 'Idade': 43},
+    {'nome': 'Bruno', 'Idade': 21},
+    {'nome': 'Thiago', 'Idade': 38},
+    {'nome': 'Fernando', 'Idade': 37},
+    {'nome': 'Edson', 'Idade': 45},
+    {'nome': 'Alexandre', 'Idade': 25},
+    {'nome': 'Jonny', 'Idade': 24},
+    {'nome': 'Murillo', 'Idade': 25}
+]
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
